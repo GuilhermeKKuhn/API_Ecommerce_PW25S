b592e273dc0708ef3056d74f8fa782f556d3e1e0
Guilherme <guilhermekurpe@gmail.com>
2024-04-03 22:13:53 -0300

    arquivos

---
+++ src/main/java/com/example/Ecommerce/ProjetoFinalEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalEcommerceApplication.class, args);
+	}
+
+}
