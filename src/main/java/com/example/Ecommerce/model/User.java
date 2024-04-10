fe2b3333bdfbd67abc8945e418b99dda4379c1c7
Guilherme <guilhermekurpe@gmail.com>
2024-04-09 22:38:53 -0300

    Classe user

--- src/main/java/com/example/Ecommerce/model/User.java
+++ src/main/java/com/example/Ecommerce/model/User.java
@@ -1,6 +1,38 @@
 package com.example.Ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "tb_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
     
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min = 4, max = 225)
+    private String nome;
+
+    @NotNull
+    @Size(min = 6)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
+    private String senha;
 }
